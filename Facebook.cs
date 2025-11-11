831e784d38b1bec26ac6604fc88b794d400715e7
angelo559 <matheus.angelo0301@gmail.com>
2025-11-11 00:01:57 +0000

    chatpoo

--- Facebook.cs
+++ Facebook.cs
@@ -4,6 +4,7 @@ public class Facebook : ChannelBase
 {
     public Facebook() : base("Facebook") { }
 
+
     public override void EnviarMensagem(string usuario, MessageBase message)
     {
         Console.WriteLine($"\n[Facebook] Enviando para o usuário {usuario}...");
